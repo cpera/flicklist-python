d423dd7452e8dde364ca21f2cad27dfe45aa548b
PERAMSETTY <cpera@monsanto.com>
2017-03-23 15:18:22 -0500

    flicklist-1 studio work commiting

--- main.py
+++ main.py
@@ -8,22 +8,22 @@ class Index(webapp2.RequestHandler):
         # TODO: make a list with at least 5 movie titles
 		 mvList = ["God Father", "Titanic", "Legend", "Avatar", "2012"]		
         # TODO: randomly choose one of the movies, and return it
-		 randVal =random.randrange(0,5)
+		 randVal =random.randrange(0,len(mvList))
 		 randomMvPick = mvList[randVal]
 		 return randomMvPick # "The Big Lebowski"
 
     def get(self):
-        # choose a movie by invoking our new function
-        movie = self.getRandomMovie()
-
-        # build the response string
-        content = "<h1>Movie of the Day</h1>"
-        content += "<p>" + movie + "</p>"
-
-        # TODO: pick a different random movie, and display it under
-        # the heading "<h1>Tommorrow's Movie</h1>"
-
-        self.response.write(content)
+        # choose a movie by invoking our new function 
+		movie = self.getRandomMovie() 
+		tomorrow_movie = self.getRandomMovie() 
+		# build the response string 
+		content = "<h1>Movie of the Day</h1>" 
+		content += "<p>" + movie + "</p>" 
+		# TODO: pick a different random movie, and display it under
+		content += "<h1>Movie for tomorrow's movie: </h1>"
+		content += "<p>" + tomorrow_movie + "</p>"
+		self.response.write(content)
+        
 
 app = webapp2.WSGIApplication([
     ('/', Index)
