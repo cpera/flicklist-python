4756fd2fc007241ba103632ff7df2efd11869b6a
PERAMSETTY <cpera@monsanto.com>
2017-04-03 17:53:30 -0500

    spaces replaced with tab

--- main.py
+++ main.py
@@ -6,17 +6,17 @@ page_header = """
 <!DOCTYPE html>
 <html>
 <head>
-    <title>FlickList</title>
-    <style type="text/css">
-        .error {
-            color: red;
-        }
-    </style>
+	<title>FlickList</title>
+	<style type="text/css">
+		.error {
+			color: red;
+		}
+	</style>
 </head>
 <body>
-    <h1>
-        <a href="/">FlickList</a>
-    </h1>
+	<h1>
+		<a href="/">FlickList</a>
+	</h1>
 """
 
 # html boilerplate for the bottom of every page
@@ -28,131 +28,131 @@ page_footer = """
 
 # a list of movies that nobody should be allowed to watch
 terrible_movies = [
-    "Gigli",
-    "Star Wars Episode 1: Attack of the Clones",
-    "Paul Blart: Mall Cop 2",
-    "Nine Lives"
+	"Gigli",
+	"Star Wars Episode 1: Attack of the Clones",
+	"Paul Blart: Mall Cop 2",
+	"Nine Lives"
 ]
 
 
 def getCurrentWatchlist():
-    """ Returns the user's current watchlist """
+	""" Returns the user's current watchlist """
 
-    # for now, we are just pretending
-    return [ "Star Wars", "Minions", "Freaky Friday", "My Favorite Martian" ]
+	# for now, we are just pretending
+	return [ "Star Wars", "Minions", "Freaky Friday", "My Favorite Martian" ]
 
 
 class Index(webapp2.RequestHandler):
-    """ Handles requests coming in to '/' (the root of our site)
-        e.g. www.flicklist.com/
-    """
+	""" Handles requests coming in to '/' (the root of our site)
+		e.g. www.flicklist.com/
+	"""
 
-    def get(self):
+	def get(self):
 
-        edit_header = "<h3>Edit My Watchlist</h3>"
+		edit_header = "<h3>Edit My Watchlist</h3>"
 
-        # a form for adding new movies
-        add_form = """
-        <form action="/add" method="post">
-            <label>
-                I want to add
-                <input type="text" name="new-movie"/>
-                to my watchlist.
-            </label>
-            <input type="submit" value="Add It"/>
-        </form>
-        """
+		# a form for adding new movies
+		add_form = """
+		<form action="/add" method="post">
+			<label>
+				I want to add
+				<input type="text" name="new-movie"/>
+				to my watchlist.
+			</label>
+			<input type="submit" value="Add It"/>
+		</form>
+		"""
 
-        # a form for crossing off movies
-        # (first we build a dropdown from the current watchlist items)
-        crossoff_options = ""
-        for movie in getCurrentWatchlist():
-            crossoff_options += '<option value="{0}">{0}</option>'.format(movie)
+		# a form for crossing off movies
+		# (first we build a dropdown from the current watchlist items)
+		crossoff_options = ""
+		for movie in getCurrentWatchlist():
+			crossoff_options += '<option value="{0}">{0}</option>'.format(movie)
 
-        crossoff_form = """
-        <form action="/cross-off" method="post">
-            <label>
-                I want to cross off
-                <select name="crossed-off-movie"/>
-                    {0}
-                </select>
-                from my watchlist.
-            </label>
-            <input type="submit" value="Cross It Off"/>
-        </form>
-        """.format(crossoff_options)
+		crossoff_form = """
+		<form action="/cross-off" method="post">
+			<label>
+				I want to cross off
+				<select name="crossed-off-movie"/>
+					{0}
+				</select>
+				from my watchlist.
+			</label>
+			<input type="submit" value="Cross It Off"/>
+		</form>
+		""".format(crossoff_options)
 
-        # if we have an error, make a <p> to display it
-        error = self.request.get("error")
-        if error:
-            error_esc = cgi.escape(error, quote=True)
-            error_element = "<p class='error'>" + error_esc + "</p>"
-        else:
-            error_element = ""
+		# if we have an error, make a <p> to display it
+		error = self.request.get("error")
+		if error:
+			error_esc = cgi.escape(error, quote=True)
+			error_element = "<p class='error'>" + error_esc + "</p>"
+		else:
+			error_element = ""
 
-        # combine all the pieces to build the content of our response
-        main_content = edit_header + add_form + crossoff_form + error_element
-        content = page_header + main_content + page_footer
-        self.response.write(content)
+		# combine all the pieces to build the content of our response
+		main_content = edit_header + add_form + crossoff_form + error_element
+		content = page_header + main_content + page_footer
+		self.response.write(content)
 
 
 class AddMovie(webapp2.RequestHandler):
-    """ Handles requests coming in to '/add'
-        e.g. www.flicklist.com/add
-    """
+	""" Handles requests coming in to '/add'
+		e.g. www.flicklist.com/add
+	"""
 
-    def post(self):
-        # look inside the request to figure out what the user typed
-        new_movie = self.request.get("new-movie")
+	def post(self):
+		# look inside the request to figure out what the user typed
+		new_movie = self.request.get("new-movie")
 
-        # if the user typed nothing at all, redirect and yell at them
-        if (not new_movie) or (new_movie.strip() == ""):
-            error = "Please specify the movie you want to add."
-            self.redirect("/?error=" + cgi.escape(error, quote=True))
+		# if the user typed nothing at all, redirect and yell at them
+		if (not new_movie) or (new_movie.strip() == ""):
+			error = "Please specify the movie you want to add."
+			self.redirect("/?error=" + cgi.escape(error, quote=True))
 
-        # if the user wants to add a terrible movie, redirect and yell at them
-        if new_movie in terrible_movies:
-            error = "Trust me, you don't want to add '{0}' to your Watchlist".format(new_movie)
-            self.redirect("/?error=" + cgi.escape(error, quote=True))
+		# if the user wants to add a terrible movie, redirect and yell at them
+		if new_movie in terrible_movies:
+			error = "Trust me, you don't want to add '{0}' to your Watchlist".format(new_movie)
+			self.redirect("/?error=" + cgi.escape(error, quote=True))
 
-        # 'escape' the user's input so that if they typed HTML, it doesn't mess up our site
-        new_movie_escaped = cgi.escape(new_movie, quote=True)
+		# 'escape' the user's input so that if they typed HTML, it doesn't mess up our site
+		new_movie_escaped = cgi.escape(new_movie, quote=True)
 
-        # build response content
-        new_movie_element = "<strong>" + new_movie_escaped + "</strong>"
-        sentence = new_movie_element + " has been added to your Watchlist!"
-        content = page_header + "<p>" + sentence + "</p>" + page_footer
-        self.response.write(content)
+		# build response content
+		new_movie_element = "<strong>" + new_movie_escaped + "</strong>"
+		sentence = new_movie_element + " has been added to your Watchlist!"
+		content = page_header + "<p>" + sentence + "</p>" + page_footer
+		self.response.write(content)
 
 
 class CrossOffMovie(webapp2.RequestHandler):
-    """ Handles requests coming in to '/cross-off'
-        e.g. www.flicklist.com/cross-off
-    """
+	""" Handles requests coming in to '/cross-off'
+		e.g. www.flicklist.com/cross-off
+	"""
 
-    def post(self):
-        # look inside the request to figure out what the user typed
-        crossed_off_movie = self.request.get("crossed-off-movie")
+	def post(self):
+		# look inside the request to figure out what the user typed
+		crossed_off_movie = self.request.get("crossed-off-movie")
 
-        if (crossed_off_movie in getCurrentWatchlist()) == False:
-            # the user tried to cross off a movie that isn't in their list,
-            # so we redirect back to the front page and yell at them
+		if (crossed_off_movie in getCurrentWatchlist()) == False:
+			# the user tried to cross off a movie that isn't in their list,
+			# so we redirect back to the front page and yell at them
 
-            # make a helpful error message
-            error = "'{0}' is not in your Watchlist, so you can't cross it off!".format(crossed_off_movie)
+			# make a helpful error message
+			error = "'{0}' is not in your Watchlist, so you can't cross it off!".format(crossed_off_movie)
 
-            # redirect to homepage, and include error as a query parameter in the URL
-            self.redirect("/?error=" + error)
+			# redirect to homepage, and include error as a query parameter in the URL
+			self.redirect("/?error=" + error)
 
-        # if we didn't redirect by now, then all is well
-        crossed_off_movie_element = "<strike>" + crossed_off_movie + "</strike>"
-        confirmation = crossed_off_movie_element + " has been crossed off your Watchlist."
-        content = page_header + "<p>" + confirmation + "</p>" + page_footer
-        self.response.write(content)
+		# if we didn't redirect by now, then all is well
+		crossed_off_movie_element = "<strike>" + crossed_off_movie + "</strike>"
+		confirmation = crossed_off_movie_element + " has been crossed off your Watchlist."
+		content = page_header + "<p>" + confirmation + "</p>" + page_footer
+		self.response.write(content)
 
 
 app = webapp2.WSGIApplication([
-    ('/', Index),
-    ('/add', AddMovie),
-    ('/cross-off', CrossOffMovie)
+	('/', Index),
+	('/add', AddMovie),
+	('/cross-off', CrossOffMovie)
 ], debug=True)
